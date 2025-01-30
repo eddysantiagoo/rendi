687ffc55582ecfc869d3d37bcf5077ca23f75daa
eddysantiagoo <eddysantiagogh@gmail.com>
2025-01-30 01:15:20 -0500

    Add utility functions, theme provider, and UI components for improved styling and theming

---
+++ app/_components/Bank/CardBank.tsx
@@ -0,0 +1,13 @@
+interface CardBankProps {
+  name: string;
+  tase: number;
+}
+
+export const CardBank = ({ name, tase }: CardBankProps) => {
+  return (
+    <div className="flex-1 p-4 border rounded shadow-lg">
+      <h1 className="text-xl font-bold mb-2">{name}</h1>
+      <p className="text-gray-700">Interest Rate: {tase}%</p>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
+import { CardBank } from "./_components/Bank/CardBank";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useId } from "react";
+
+const banksInfo = [
+  { name: "Banco Azteca", tase: 4.5 },
+  { name: "BanCoppel", tase: 4.5 },
+  { name: "Banorte", tase: 4.5 },
+  { name: "BBVA", tase: 4.5 },
+  { name: "Bancolombia", tase: 13 },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className=" place-content-center place-items-center px-48">
+      <ModeToggle />
+      <h1 className="text-2xl bg-[#122322] text-[#00d992] py-2 px-6 my-2 rounded-md font-bold">
+        Calcula los rendimientos
+      </h1>
+      <p>Para tus cuentas de ahorro</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex w-full my-12">
+        {banksInfo.map((banksInfo, index) => (
+          <CardBank key={index} {...banksInfo} />
+        ))}
+      </div>
+
+      <section className="grid grid-cols-2 w-full">
+        <div className="bg-neutral-900 flex flex-col p-12">
+          <div>
+            <Label htmlFor={"mijo"}>Monto</Label>
+            <Input autoFocus id={"mijo"} placeholder="Email" type="email" />
+            <p
+              className="mt-2 text-xs text-muted-foreground"
+              role="region"
+              aria-live="polite"
+            >
+              Es el monto que deseas invertir
+            </p>
+          </div>
+          <div>
+            <Label htmlFor={"mijo"}>Meses</Label>
+            <Input id={"mijo"} placeholder="Email" type="email" />
+            <p
+              className="mt-2 text-xs text-muted-foreground"
+              role="region"
+              aria-live="polite"
+            >
+              Es el tiempo en que tendras tu dinero invertido
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="p-2 border border-neutral-900">
+          <h1 className="text-center text-2xl font-bold">Resultados</h1>
+
+          <div className="p-12">
+            {banksInfo.map((banksInfo, index) => (
+              <CardBank key={index} {...banksInfo} />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
