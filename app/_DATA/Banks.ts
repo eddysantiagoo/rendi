f530e379eb032183744939b1c2e7e9c705cc561e
eddysantiagoo <eddysantiagogh@gmail.com>
2025-04-06 21:29:11 -0500

    feat: update uala tasa ea

--- app/_DATA/Banks.ts
+++ app/_DATA/Banks.ts
@@ -1,14 +1,16 @@
+
+
 export const Banks = [
   { name: "Nubank", tasaEA: 9.5, image: "/nubank.webp", id: 1, act: true },
   { name: "Bancolombia", tasaEA: 0.05, image: "/bancolombia.webp", id: 2, },
   { name: "Lulo Bank", tasaEA: 10, image: "/lulo.webp", id: 3 },
   { name: "Banco Finandina", tasaEA: 10.50, image: "/finandina.webp", id: 9, type:"Cuenta Flexidigital+" },
-  { name: "RappiPay", tasaEA: 10, image: "/rappi.jpg", id: 4, act: true },
+  { name: "RappiPay", tasaEA: 10, image: "/rappi.jpg", id: 4 },
   { name: "Bancamia", tasaEA: 10.5, image: "/bancamia.webp", id: 5, type:"Cuenta RentaPlus" },
   { name: "Pibank", tasaEA: 10, image: "/pibank.webp", id: 8 },
 ];
   
 export const DepositosBajoMonto = [
-  { name: "Ualá", tasaEA: 13, image: "/uala.webp", type: "Deposito bajo monto", id: 6 },
+  { name: "Ualá", tasaEA: 12, image: "/uala.webp", type: "Deposito bajo monto", id: 6, act: true },
   { name: "Nequi", tasaEA: 0.1, image: "/nequi.webp", type: "Deposito bajo monto", id: 7 },
 ];
