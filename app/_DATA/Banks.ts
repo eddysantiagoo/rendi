d9e5f5e52ef67eb4a87bf9dcc37e6ecbebc4e4c7
eddysantiagoo <eddysantiagogh@gmail.com>
2025-04-13 23:21:06 -0500

    fix: update RappiPay tasaEA value and remove unused 'act' property from Nubank entry

--- app/_DATA/Banks.ts
+++ app/_DATA/Banks.ts
@@ -1,11 +1,11 @@
 
 
 export const Banks = [
-  { name: "Nubank", tasaEA: 9.5, image: "/nubank.webp", id: 1, type:"Cuenta de ahorros", act: true },
+  { name: "Nubank", tasaEA: 9.5, image: "/nubank.webp", id: 1, type:"Cuenta de ahorros"},
   { name: "Bancolombia", tasaEA: 0.05, image: "/bancolombia.webp", id: 2, },
   { name: "Lulo Bank", tasaEA: 10, image: "/lulo.webp", id: 3 },
   { name: "Banco Finandina", tasaEA: 10.50, image: "/finandina.webp", id: 9, type:"Cuenta Flexidigital+" },
-  { name: "RappiPay", tasaEA: 10, image: "/rappi.jpg", id: 4 },
+  { name: "RappiPay", tasaEA: 9, image: "/rappi.jpg", id: 4 },
   { name: "Bancamia", tasaEA: 10.5, image: "/bancamia.webp", id: 5, type:"Cuenta RentaPlus" },
   { name: "Pibank", tasaEA: 10, image: "/pibank.webp", id: 8 },
 ];
