b38e7a19823e83dc5b74ba0d87ab3661bc17a163
eddysantiagoo <eddysantiagogh@gmail.com>
2025-04-20 22:20:02 -0500

    fix: add 'act' property to RappiPay entry in Banks data

--- app/_DATA/Banks.ts
+++ app/_DATA/Banks.ts
@@ -5,7 +5,7 @@ export const Banks = [
   { name: "Bancolombia", tasaEA: 0.05, image: "/bancolombia.webp", id: 2, },
   { name: "Lulo Bank", tasaEA: 10, image: "/lulo.webp", id: 3 },
   { name: "Banco Finandina", tasaEA: 10.50, image: "/finandina.webp", id: 9, type:"Cuenta Flexidigital+" },
-  { name: "RappiPay", tasaEA: 9, image: "/rappi.jpg", id: 4 },
+  { name: "RappiPay", tasaEA: 9, image: "/rappi.jpg", id: 4, act: true },
   { name: "Bancamia", tasaEA: 10.5, image: "/bancamia.webp", id: 5, type:"Cuenta RentaPlus" },
   { name: "Pibank", tasaEA: 10, image: "/pibank.webp", id: 8 },
 ];
